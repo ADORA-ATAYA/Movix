68f0793b212cac6a7d7481d63eb53f4bff389f58
ADORA-ATAYA <pragyanshutayal2100@gmail.com>
2024-04-01 17:11:09 +0530

    title fix

--- src/pages/details/videosSection/VideosSection.jsx
+++ src/pages/details/videosSection/VideosSection.jsx
@@ -37,7 +37,7 @@ const VideosSection = ({ data, loading }) => {
                                             <Img src={`https://img.youtube.com/vi/${video.key}/mqdefault.jpg`} />
                                             <PlayIcon />
                                         </div>
-                                        <div className="vedioTitle">{video.name}</div>
+                                        <div className="videoTitle">{video.name}</div>
                                     </div>
                                 ))
                             }
